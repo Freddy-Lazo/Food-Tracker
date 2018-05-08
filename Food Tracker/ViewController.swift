b5a0339128aaee7cb1603dc46666d181fae37ab0
Freddy Lazo <freddy.lazo@pucp.pe>
2018-05-08 11:36:39 -0500

    Initial Commit

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Freddy Lazo on 5/8/18.
+//  Copyright © 2018 Freddy Lazo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
